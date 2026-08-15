00979d5d258bc96331e07240f990c3b00a40644d
Kavish-Y <kavishyadav994@gmail.com>
2026-08-15 22:41:46 +0530

    Create p-1-v.cpp

---
+++ leetcode/P-1-ARR/p-1-v.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
